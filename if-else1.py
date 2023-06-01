37c926fdb62740564c6e32e396be4499d2fcd849
chetan930928 <123301781+chetan930928@users.noreply.github.com>
2023-05-31 22:54:03 -0700

    Create if-else1.py

---
+++ if-else1.py
@@ -0,0 +1,10 @@
+print ("hello Chetan Gavande")
+print ("Welcome To Google App Engine ")
+print ("............................................")
+print ("Write a simple If-else program ")
+a =10
+if(a%2==0)
+print ("Number is even ")
+else
+print ("Number is odd ")
+print ("End The Program")
