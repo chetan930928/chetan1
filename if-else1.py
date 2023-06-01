0b5c987e19bc738be51ab70172487d95a8621f6c
chetan930928 <123301781+chetan930928@users.noreply.github.com>
2023-05-31 22:58:22 -0700

    Update if-else1.py

--- if-else1.py
+++ if-else1.py
@@ -3,8 +3,8 @@ print ("Welcome To Google App Engine ")
 print ("............................................")
 print ("Write a simple If-else program ")
 a =10
-if(a%2)==0
+if(a%2)==0:
 print ("Number is even ")
-else
+else:
 print ("Number is odd ")
 print ("End The Program")
